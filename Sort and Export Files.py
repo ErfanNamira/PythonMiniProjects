5185570fa51f1057d333357a448e19823b84c18d
Namira <115206959+ErfanNamira@users.noreply.github.com>
2023-09-14 13:28:44 +0330

    Update Sort and Export Files.py

--- Sort and Export Files.py
+++ Sort and Export Files.py
@@ -1,30 +1,36 @@
 # Sort and Export Files
 # Python Mini Projects
 # ErfanNamira
-# https://github.com/ErfanNamira/Python
+# https://github.com/ErfanNamira/PythonMiniProjects
 
 import os
 import glob
 
-# Ask the user for the folder path
-folder_path = input("Enter the folder path: ")
+while True:
+    # Ask the user for the folder path
+    folder_path = input("Enter the folder path (or type 'exit' to quit): ")
 
-# Get a list of all files in the folder
-file_list = glob.glob(os.path.join(folder_path, "*"))
+    # Check if the user wants to exit
+    if folder_path.lower() == 'exit':
+        break
 
-# Sort the file list alphabetically
-file_list.sort()
+    # Get a list of all files in the folder
+    file_list = glob.glob(os.path.join(folder_path, "*"))
 
-# Ask the user for the output TXT file name (with .txt extension)
-output_file = input("Enter the TXT file name (e.g., sorted_files.txt): ")
+    # Sort the file list alphabetically
+    file_list.sort()
 
-# Add .txt extension if not provided by the user
-if not output_file.endswith(".txt"):
-    output_file += ".txt"
+    # Ask the user for the output TXT file name (with .txt extension)
+    output_file = input("Enter the TXT file name (e.g., sorted_files.txt): ")
 
-# Write the sorted file list to the TXT file
-with open(output_file, "w") as f:
-    for file_name in file_list:
-        f.write(file_name + "\n")
+    # Add .txt extension if not provided by the user
+    if not output_file.endswith(".txt"):
+        output_file += ".txt"
 
-print(f"List of files has been sorted and saved to {output_file}.")
+    # Write the sorted file list to the TXT file
+    with open(output_file, "w") as f:
+        for file_name in file_list:
+            f.write(file_name + "\n")
+
+    print(f"List of files has been sorted and saved to {output_file}.")
+    input("Press Enter to continue...")
